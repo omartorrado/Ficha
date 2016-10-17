2046a011bd7aaba228d4e3bfff727da2cf7ce9a3
otorradomiguez <otorradomiguez@nautilus28.danielcastelao.org>
2016-10-17 14:15:58 +0200

    trabajando en los metodos de tiradas.java

--- src/proyecto_ficha/Tiradas.java
+++ src/proyecto_ficha/Tiradas.java
@@ -43,8 +43,19 @@ public class Tiradas {
         for (int i=1;i<dados+1;i++){
             resultado=resultado+rnd.nextInt(caras)+1;
         }
-        //System.out.println(resultado);
+        System.out.print(resultado);
         resultado=resultado/dados;
+        return resultado;
+        }
+        
+        public int roll_media_media(int dados, int caras){
+        int resultado=0;
+        float result=0;
+        for (int i=1;i<dados+1;i++){
+            resultado=resultado+rnd.nextInt(caras)+1;
+        }
+        System.out.print(resultado);
+        resultado=(int) Math.round((float)resultado/(float)dados);
         return resultado;
         }
         
@@ -54,8 +65,23 @@ public class Tiradas {
         for (int i=1;i<dados+1;i++){
             resultado=resultado+rnd.nextInt(caras)+1;
         }
-        //System.out.println(resultado);
-        resultado=(int) Math.round((float)resultado/(float)dados);
+        System.out.print(resultado);
+        resultado=(int) Math.ceil((float)resultado/(float)dados);
         return resultado;
+        }
+        
+        public ArrayList roll_alto(int dados, int caras, int cuantos) {
+        ArrayList tirada= new ArrayList();
+        int resultado;
+        for (int i=1; i<dados+1; i++){
+            resultado = rnd.nextInt(caras)+1;
+            tirada.add(resultado);
+            }
+        tirada.sort(null);
+        Collections.reverse(tirada);
+        for (int o=dados-1;o>=cuantos;o--){
+        tirada.remove(o);
+        }
+        return tirada;
         }
 }
